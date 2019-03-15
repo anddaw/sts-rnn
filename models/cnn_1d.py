7e943ddd0bdbbaa8daec95c374b87254b35713d5
Andrzej Dawidziuk <andrzej.dawidziuk@gmail.com>
2019-03-15 18:14:39 +0100

    CNN_1D implementation

---
+++ models/cnn_1d.py
@@ -0,0 +1,42 @@
+from typing import Tuple, Any
+from torch import nn
+import torch
+import torch.nn.functional as F
+from models.base import BaseModel
+
+
+class CNN1d(BaseModel):
+
+    def __init__(self, embedding_size: int, output_size: int, sentence_length: int = 30):
+        super(CNN1d, self).__init__()
+
+        self.output_size = output_size
+
+        self.sentence_length = sentence_length
+        self.cnn_l = nn.Conv1d(1, embedding_size, embedding_size, stride=embedding_size)
+        self.cnn_r = nn.Conv1d(1, embedding_size, embedding_size, stride=embedding_size)
+
+        self.pool = nn.MaxPool1d(self.sentence_length)
+        self.linear_1 = nn.Linear(embedding_size * 2, embedding_size)
+        self.linear_2 = nn.Linear(embedding_size, output_size)
+
+    def forward(self, sentence_pair: Tuple[Any, Any]) -> torch.Tensor:
+        sentence_l, sentence_r = sentence_pair
+
+        sentence_l = F.pad(sentence_l, (0, 0, 0, self.sentence_length-sentence_l.shape[0]))
+        sentence_l = sentence_l.view(1, 1, -1)
+        x_l = F.relu(self.cnn_l(sentence_l))
+        x_l = self.pool(x_l)
+        v_l = x_l.view(-1)
+
+        sentence_r = F.pad(sentence_r, (0, 0, 0, self.sentence_length - sentence_r.shape[0]))
+        sentence_r = sentence_r.view(1, 1, -1)
+        x_r = F.relu(self.cnn_r(sentence_r))
+        x_r = self.pool(x_r)
+        v_r = x_r.view(-1)
+
+        x = torch.cat((torch.abs(v_l - v_r), v_l * v_r)).view(1, -1)
+        x = torch.tanh(self.linear_1(x))
+        x = F.softmax(self.linear_2(x))
+
+        return x
--- models/pick_model.py
+++ models/pick_model.py
@@ -1,15 +1,20 @@
 from typing import Optional
 
 from models.base import BaseModel
+from models.cnn_1d import CNN1d
 from models.vrnn_linear import VRNNLinear
 from models.vrnn_tree import VRNNTree
 
 
 def pick_model(config) -> Optional[BaseModel]:
     num_layers = config['num_rnn_layers'] if 'num_rnn_layers' in config else 1
-    if config['input_type'] == 'sentence':
+    if config['model'] == 'vrnn':
+        if config['input_type'] == 'sentence':
 
-        return VRNNLinear(hidden_size=50, embedding_size=50, output_size=6, num_layers=num_layers)
+            return VRNNLinear(hidden_size=50, embedding_size=50, output_size=6, num_layers=num_layers)
 
-    elif config['input_type'] == 'tree':
-        return VRNNTree(hidden_size=50, embedding_size=50, output_size=6, num_layers=num_layers)
+        elif config['input_type'] == 'tree':
+            return VRNNTree(hidden_size=50, embedding_size=50, output_size=6, num_layers=num_layers)
+
+    elif config['model'] == 'cnn_1d':
+        return CNN1d(embedding_size=50, output_size=6, sentence_length=config['sentence_length'])
