4a4eb42e3a60a1be8bc729ff554594ff7aa2d1df
Andrzej Dawidziuk <andrzej.dawidziuk@gmail.com>
2019-03-18 19:01:36 +0100

    Cnn2D initial implementation

---
+++ models/cnn_2d.py
@@ -0,0 +1,51 @@
+from typing import Tuple, Any
+from torch import nn
+import torch
+import torch.nn.functional as F
+from models.base import BaseModel
+
+
+class CNN2d(BaseModel):
+
+    def __init__(self, embedding_size: int, output_size: int, sentence_length: int = 32):
+        super(CNN2d, self).__init__(output_size)
+
+        self.sentence_length = sentence_length
+
+        self.out_channels_layer_1 = 100
+        self.out_channels_layer_2 = 100
+
+        self.cnn_1 = nn.Conv2d(in_channels=embedding_size*2, out_channels=self.out_channels_layer_1,
+                               padding=1, kernel_size=3)
+        self.max_pool_1 = nn.MaxPool2d(kernel_size=4)
+
+        self.cnn_2 = nn.Conv2d(in_channels=self.out_channels_layer_1, out_channels=self.out_channels_layer_2,
+                               padding=1, kernel_size=3)
+
+        self.classifier = nn.Linear(in_features=self.out_channels_layer_2, out_features=output_size)
+
+    def forward(self, sentence_pair: Tuple[Any, Any]) -> torch.Tensor:
+        sentence_l, sentence_r = sentence_pair
+
+        sentence_l = F.pad(sentence_l, (0, 0, 0, self.sentence_length-sentence_l.shape[0]))
+        sentence_l = sentence_l.t()
+
+        sentence_r = F.pad(sentence_r, (0, 0, 0, self.sentence_length - sentence_r.shape[0]))
+        sentence_r = sentence_r.t()
+
+        x_l = sentence_l.unsqueeze(1).repeat((1, self.sentence_length, 1))
+        x_r = sentence_r.unsqueeze(2).repeat((1, 1, self.sentence_length))
+
+        x = torch.cat((x_l, x_r), 0).view(1, -1, self.sentence_length, self.sentence_length)
+
+        x = F.relu(self.cnn_1(x))
+        x = self.max_pool_1(x)
+        x = F.relu(self.cnn_2(x))
+
+        x = F.max_pool2d(x, kernel_size=x.shape[3])
+
+        x = x.view(-1, self.out_channels_layer_2)
+
+        x = F.softmax(self.classifier(x))
+
+        return x
--- models/pick_model.py
+++ models/pick_model.py
@@ -3,24 +3,33 @@ from typing import Optional
 from models.base import BaseModel
 from models.cnn_1d import CNN1d
 from models.cnn_2d_densenet import CNN2dDenseNet
+from models.cnn_2d import CNN2d
 from models.vrnn_linear import VRNNLinear
 from models.vrnn_tree import VRNNTree
 
 
-def pick_model(config) -> Optional[BaseModel]:
+def pick_model(config, embedding_size=50, output_size=6) -> Optional[BaseModel]:
     num_layers = config['num_rnn_layers'] if 'num_rnn_layers' in config else 1
     if config['model'] == 'vrnn':
         if config['input_type'] == 'sentence':
 
-            return VRNNLinear(hidden_size=50, embedding_size=50, output_size=6, num_layers=num_layers)
+            return VRNNLinear(hidden_size=50, embedding_size=embedding_size, output_size=output_size,
+                              num_layers=num_layers)
 
         elif config['input_type'] == 'tree':
-            return VRNNTree(hidden_size=50, embedding_size=50, output_size=6, num_layers=num_layers)
+            return VRNNTree(hidden_size=50, embedding_size=embedding_size, output_size=output_size,
+                            num_layers=num_layers)
 
     elif config['model'] == 'cnn_1d':
-        return CNN1d(embedding_size=50, output_size=6, sentence_length=config['sentence_length'])
+        return CNN1d(embedding_size=embedding_size, output_size=output_size,
+                     sentence_length=config['sentence_length'])
 
     elif config['model'] == 'cnn_2d_densenet':
 
-        return CNN2dDenseNet(embedding_size=50, output_size=6, sentence_length=config['sentence_length'])
+        return CNN2dDenseNet(embedding_size=embedding_size, output_size=output_size,
+                             sentence_length=config['sentence_length'])
+
+    elif config['model'] == 'cnn_2d':
+
+        return CNN2d(embedding_size=embedding_size, output_size=output_size, sentence_length=config['sentence_length'])
 
